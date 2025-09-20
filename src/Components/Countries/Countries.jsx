b994a52fdc25c295cfd4c0e78f83c1cde49084ad
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-20 12:34:53 +0600

    react-word

---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>React World on The Go...</h2>
+        </div>
+    );
+};
+
+export default Countries;
